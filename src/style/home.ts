1e233029edb60799ecf7c04a1362560348832010
lara-vian <lara.vsteinbach@gmail.com>
2023-10-03 19:48:13 -0300

    add styled components

---
+++ src/style/home.ts
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+
+export const Background = styled.div`
+	flex: 1;
+	background-color: ${props=>props.theme.THEME.PRETO};
+`
---
+++ src/theme/index.ts
@@ -0,0 +1,7 @@
+export const THEME = {
+	"ROXO":"#bd93f9",
+	"CIANO":"#8be9fd",
+	"PINK":"#ff79c6",
+	"BRANCO":"#ffff",
+	"PRETO":"#282a36"
+}
