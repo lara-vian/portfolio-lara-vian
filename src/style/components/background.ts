4019afcca3d31c40762813dfab1a27ebd05ab529
lara-vian <lara.vsteinbach@gmail.com>
2023-10-14 16:15:43 -0300

    finish custom button

--- src/style/components/background.ts
+++ src/style/components/background.ts
@@ -16,16 +16,35 @@ export const NavBar = styled.nav`
 	border-bottom: 0.15rem solid ${props=>props.theme.roxo};
 `
 export const Button = styled.button<{$active:boolean}>`
-	margin: 0.5em;
-	padding: 0.5rem;
-	font-size: 2em;
-	color: ${props=>props.$active ? props.theme.preto : props.theme.branco};
-	border: 0.15rem solid ${props=>props.theme.roxo};
-	background: ${props=> props.$active ? props.theme.roxo : "transparent"};
+	display: inline-block;
+	margin: 1rem;
+	padding: 0.9rem 1.8rem;
+	font-size: 1.5rem;
+	color: ${props=> props.$active ? props.theme.preto : props.theme.branco};
+	border: 3px solid ${props=> props.theme.roxo};
+	cursor: pointer;
+	position: relative;
+	background-color: ${props=> props.$active ? props.theme.roxo : "transparent"};
 	text-decoration: none;
+	overflow: hidden;
+	z-index: 1;
+	&::before {
+		content: "";
+		position: absolute;
+		left: 0;
+		top: 0;
+		width: 100%;
+		height: 100%;
+		background-color: ${props=> props.theme.roxo};
+		transform: translateX(-100%);
+		transition: all .3s;
+		z-index: -1;
+	}
+	
+	&:hover::before {
+		transform: translateX(0);
+	}
 	&:hover{
-		color: ${props=>props.theme.preto};
-		background-color: ${props=>props.theme.roxo};
-		transition: all .5s;
+		color: ${props=> props.theme.preto};
 	}
 `
